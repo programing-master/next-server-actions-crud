91e5295cbce0e8f3aa2ed4753fa17e47bade0534
Javier Valladares Alonso <javierprofesionalwork@gmail.com>
2025-02-15 11:22:48 -0500

    Update eslint.config.mjs

--- catalog/eslint.config.mjs
+++ catalog/eslint.config.mjs
@@ -4,13 +4,17 @@ import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = {
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  rules: {
+    // Desactiva las reglas específicas que mencionaste
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+};
 
 export default eslintConfig;
